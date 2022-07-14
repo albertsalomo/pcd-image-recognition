c47a8d592d94c030e50dbea34da6157e7ef36953
Baptista VY <bvarani2172@gmail.com>
2022-07-14 20:46:18 +0700

    edit button navbar

--- pcdWebsite/template/aboutus.php
+++ pcdWebsite/template/aboutus.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+{% if theme %}
+<html lang="en" data-theme="{{ theme }}">
+{% else %}
+<html lang="en" data-theme="">
+{% endif %}
 
 <head>
     {% include "header.php" %}
@@ -11,7 +15,7 @@
     {% load static %}
 
     <div class="container">
-        <h4 class="pt-5" style="text-align: center;">OUR TEAM</h4>
+        <h4 class="pt-5" style="text-align: center; padding-bottom: 5%">OUR TEAM</h4>
 
         <!-- <div class="d-flex justify-content-center card-deck pt-5">
             <div class="card" style="width: 18rem;">
@@ -42,7 +46,7 @@
 
         <div class="card mb-3 rounded-pill border">
             <div class="d-flex align-items-center">
-                <div class="col col-9 d-flex flex-column align-self-stretch align-items-center justify-content-center h-100 p-2">
+                <div class="col col-9 d-flex flex-column align-items-center justify-content-center h-100">
                     <h1>Albert Salomo</h1>
                     <h3>412019040</h3>
                     <p>"it's just a beningning."</p>
@@ -52,8 +56,44 @@
                 </div>
             </div>
         </div>
+        <div class="card mb-3 rounded-pill border">
+            <div class="d-flex align-items-center">
+                <div class="col col-3 d-flex justify-content-center p-3" style="text-align: center;">
+                    <img class="card-img-top img-fluid rounded-circle" src="{% static 'pcdWebsite/images/member/1x1/BaptistaVY.png' %}" alt="Card image cap">
+                </div>
+                <div class="col col-9 d-flex flex-column align-items-center justify-content-center h-100">
+                    <h1>Baptista Varani Yoenata</h1>
+                    <h3>412019019</h3>
+                    <p>"it's just a beningning."</p>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 rounded-pill border">
+            <div class="d-flex align-items-center">
+                <div class="col col-9 d-flex flex-column align-items-center justify-content-center h-100">
+                    <h1>Jessica Octaviany</h1>
+                    <h3>412019009</h3>
+                    <p>"it's just a beningning."</p>
+                </div>
+                <div class="col col-3 d-flex justify-content-center p-3" style="text-align: center;">
+                    <img class="card-img-top img-fluid rounded-circle" src="{% static 'pcdWebsite/images/member/1x1/Jessica_Octaviany.png' %}" alt="Card image cap">
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 rounded-pill border">
+            <div class="d-flex align-items-center">
+                <div class="col col-3 d-flex justify-content-center p-3" style="text-align: center;">
+                    <img class="card-img-top img-fluid rounded-circle" src="{% static 'pcdWebsite/images/member/1x1/Oscar_Deladas.png' %}" alt="Card image cap">
+                </div>
+                <div class="col col-9 d-flex flex-column align-items-center justify-content-center h-100">
+                    <h1>Oscar Deladas</h1>
+                    <h3>412019037</h3>
+                    <p>"it's just a beningning."</p>
+                </div>
+            </div>
+        </div>
 
-        <div class="container">
+        <!-- <div class="container">
             <div class="row">
                 <div class="col" style="text-align: center;">
                     <h2>Albert Salomo</h2>
@@ -107,11 +147,11 @@
                     <p>Contribution : asd</p>
                 </div>
             </div>
-        </div>
+        </div> -->
 
         <hr class="m-5">
 
-        <p style="text-align: justify;">Hello! We are from team <b>Semoga A</b>. Our team members are shown on the picture above. This is our project from Digital Image Recognition subject.
+        <p style="text-align: justify; padding-bottom: 5%;">Hello! We are from team <b>Semoga A</b>. Our team members are shown on the picture above. This is our project from Digital Image Recognition subject.
             This website was made for medical detection, especially lungs. So the visitor only has to submit the medical scans of lungs and the machine learning will start detecting.
             The result will be shown in the same page, below the picture that submitted before. This machine learning can detect 3 types of lungs scans. Covid-19, Normal, and Pneumonia. For the reference, please check the <b>CONTENT</b> page.
             Visitor can read and learn more about the machine learning that is implemented in this websites. About how the machine learning works and how it can determine the diseases</p>
